768a0f6a172c2755379ce0c275a6266cd7e8cc1b
linfuan <18355022382@163.com>
2025-07-26 23:13:23 +0800

    签到任务，领取sku和积分，并且发送mq

---
+++ big-market/big-market-domain/src/main/java/com/lin/domain/rebate/repository/IBehaviorRebateRepository.java
@@ -0,0 +1,25 @@
+package com.lin.domain.rebate.repository;
+
+import com.lin.domain.rebate.model.aggregate.BehaviorRebateAggregate;
+import com.lin.domain.rebate.model.valobj.BehaviorTypeVO;
+import com.lin.domain.rebate.model.valobj.DailyBehaviorRebateVO;
+
+import java.util.List;
+
+/**
+ * @author Fuzhengwei bugstack.cn @小傅哥
+ * @description 行为返利服务仓储接口
+ * @create 2024-04-30 14:58
+ */
+public interface IBehaviorRebateRepository {
+
+    /**
+     * 签到可能返利的类型有，增加抽奖额度以及积分
+     * @param behaviorTypeVO
+     * @return
+     */
+    List<DailyBehaviorRebateVO> queryDailyBehaviorRebateConfig(BehaviorTypeVO behaviorTypeVO);
+
+    void saveUserRebateRecord(String userId, List<BehaviorRebateAggregate> behaviorRebateAggregates);
+
+}
